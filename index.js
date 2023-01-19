8cd30aef2f870a97a53cc591913febf1bccca06f
Veridiana Maricato <maricato.veridiana@gmail.com>
2023-01-19 14:22:48 -0300

    status added

--- index.js
+++ index.js
@@ -9,7 +9,21 @@ app.use(
 
 app.use(express.json())
 
+app.post('/createproduct', (req, res) => {
+    const name = req.body.name
+    const price = req.body.price
+
+    if(!name){
+        res.status(422).json({message: 'O campo nome é obrigatório!'})
+        return
+    }
+
+    console.log(name)
+    console.log(price)
+    res.status(201).json({message: `O produto ${name} foi criado com sucesso!`})
+})
+
 app.get('/', (req,res) => {
-    res.json({message: 'primeira rota criada com sucesso'})
+    res.status(200).json({message: 'primeira rota criada com sucesso'})
 })
 app.listen(3000)
